9ac5d909157bf564ecc47f810c7396da31c98180
lkl <1691487896@qq.com>
2024-07-18 20:35:18 +0800

    完成了详情页的布局

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -3,6 +3,8 @@ import { GetGoodsAPI } from '@/services/goods'
 import type { GoodsResult } from '@/types/goods'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
+// 获取屏幕边界到安全区域距离
+const { safeAreaInsets } = uni.getSystemInfoSync()
 // 指示点准备
 const currentIndex = ref(0)
 // 接收页面参数
@@ -52,7 +54,90 @@ const onTapImage = (item: string) => {
         </view>
       </view>
     </view>
+    <!-- 商品简介 -->
+    <view class="meta">
+      <view class="price">
+        <text class="symbol">¥</text>
+        <text class="number">123</text>
+      </view>
+      <view class="name ellipsis">名字</view>
+      <view class="desc">简介</view>
+    </view>
+    <!-- 操作面板 -->
+    <view class="action">
+      <view class="item">
+        <text class="label">选择</text>
+        <view class="box">
+          <text class="text">请选择商品规格</text>
+          <uni-icons type="right" size="20"></uni-icons>
+        </view>
+      </view>
+      <view class="item">
+        <text class="label">送至</text>
+        <view class="box">
+          <text class="text">请选择收获地址</text>
+          <uni-icons type="right" size="20"></uni-icons>
+        </view>
+      </view>
+      <view class="item">
+        <text class="label">服务</text>
+        <view class="box">
+          <view class="abc">无忧退 快速退款 免费包邮</view>
+          <uni-icons type="right" size="18"></uni-icons>
+        </view>
+      </view>
+    </view>
+    <!-- 商品详情 -->
+    <view class="detail">
+      <view class="title">
+        <text class="text">详情</text>
+      </view>
+      <view class="content">
+        <view class="item">
+          <text class="label">属性</text>
+          <text class="value">介绍1231231231231231</text>
+        </view>
+      </view>
+      <!-- 图片详情 -->
+      <image></image>
+    </view>
+    <!-- 同类推荐 -->
+    <view class="similar">
+      <view class="title">
+        <text class="text">同类推荐</text>
+      </view>
+    </view>
+    <view class="content">
+      <navigator class="goods" v-for="item in 8" :key="item">
+        <image class="image" mode="aspectFill"></image>
+        <view class="name ellipsis">介绍</view>
+        <view class="price">
+          <text class="symbol">¥</text>
+          <text class="number">5</text>
+        </view>
+      </navigator>
+    </view>
   </scroll-view>
+  <view class="footer" :style="{ paddingBottom: safeAreaInsets?.bottom + 'px' }">
+    <view class="icons">
+      <view class="icomItem">
+        <uni-icons type="heart" size="20"></uni-icons>
+        <view>收藏</view>
+      </view>
+      <view class="icomItem">
+        <uni-icons type="chatbubble" size="20"></uni-icons>
+        <view>客服</view>
+      </view>
+      <view class="icomItem">
+        <uni-icons type="cart" size="20"></uni-icons>
+        <view>购物车</view>
+      </view>
+    </view>
+    <view class="buttons">
+      <view class="addcart">加入购物车</view>
+      <view class="payment">立即购买</view>
+    </view>
+  </view>
 </template>
 
 <style lang="scss">
@@ -85,6 +170,169 @@ page {
       .split {
         margin: 0 1rpx 0 2rpx;
       }
+    }
+  }
+}
+.meta {
+  border-bottom: 1rpx solid #eaeaea;
+  background-color: #fff;
+  .price {
+    padding: 35rpx 30rpx;
+    color: #fff;
+    font-size: 34rpx;
+    box-sizing: border-box;
+    background-color: #35c8a9;
+    .number {
+      font-size: 40rpx;
+    }
+  }
+  .name {
+    margin: 20rpx;
+    font-size: 32rpx;
+    color: #333;
+  }
+  .desc {
+    padding: 0 20rpx 30rpx;
+    font-size: 24rpx;
+    color: #cf4444;
+  }
+}
+.action {
+  padding: 0 20rpx;
+  background-color: #fff;
+  .item {
+    border-bottom: 1px solid #eaeaea;
+    padding: 25rpx 0;
+    display: flex;
+    align-items: center;
+    .label {
+      font-size: 25rpx;
+      padding-right: 25rpx;
+    }
+    .box {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      flex: 1;
+      .abc {
+        font-size: 26rpx;
+      }
+    }
+  }
+}
+.detail {
+  padding-left: 20rpx;
+  background-color: #fff;
+  margin: 25rpx 0;
+  .title {
+    padding: 25rpx 0;
+    font-weight: 600;
+    font-size: 28rpx;
+    .text {
+      padding-left: 10rpx;
+      border-left: 5rpx #27ba9b solid;
+    }
+  }
+  .content {
+    background-color: #fff;
+
+    .item {
+      border-bottom: 1rpx solid #eaeaea;
+      padding: 10rpx 0;
+      font-size: 24rpx;
+      color: #333;
+      display: flex;
+      line-height: 2;
+      width: 100%;
+      .label {
+        width: 200rpx;
+      }
+      .value {
+        flex: 1;
+      }
+    }
+  }
+}
+.similar {
+  background-color: #fff;
+  padding-left: 20rpx;
+  .title {
+    padding: 25rpx 0;
+    font-weight: 600;
+    font-size: 28rpx;
+    .text {
+      padding-left: 10rpx;
+      border-left: 5rpx #27ba9b solid;
+    }
+  }
+}
+.content {
+  padding: 0 20rpx 20rpx 20rpx;
+  background-color: #f4f4f4;
+  overflow: hidden;
+  navigator {
+    width: 335rpx;
+    padding: 20rpx;
+    margin: 20rpx 10rpx;
+    border-radius: 10rpx;
+    background-color: #fff;
+    float: left;
+    .image {
+      height: 260rpx;
+    }
+    .name {
+      margin: 10rpx 0;
+      font-size: 26rpx;
+      color: #262626;
+    }
+    .price {
+      line-height: 1;
+      font-size: 20rpx;
+      color: #cf4444;
+    }
+    .number {
+      font-size: 26rpx;
+      margin-left: 2rpx;
+    }
+  }
+}
+.footer {
+  background: #fff;
+  height: 100rpx;
+  padding: 0 20rpx;
+  border-top: 1px solid #eaeaea;
+  display: flex;
+  box-sizing: content-box;
+  .icons {
+    display: flex;
+    align-items: center;
+    .icomItem {
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+      padding: 0 12rpx;
+    }
+  }
+  .buttons {
+    display: flex;
+    align-items: center;
+    flex: 1;
+    justify-content: space-evenly;
+    .addcart,
+    .payment {
+      width: 200rpx;
+      text-align: center;
+      line-height: 72rpx;
+      font-size: 26rpx;
+      color: #fff;
+      background-color: #ffa868;
+      border-radius: 72rpx;
+    }
+    .addcart {
+      padding-right: 10rpx;
+    }
+    .payment {
+      background-color: #27ba9b;
     }
   }
 }
